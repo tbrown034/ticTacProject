525a242c9f95b1a6b0b5d3d50dc0e09aefd00e4b
tbrown034 <tbrown034@gmail.com>
2022-08-21 12:14:58 -0500

    not broken

--- app.js
+++ app.js
@@ -2,6 +2,11 @@
 
 // const state = {}
 
+// const player1Name = document.getElementById("enterPlayer1").value;
+// console.log(player1Name)
+// const player2Name = document.getElementById("enterPlayer2").value;
+
+
 // const initialState = {
 //     players: ['x', 'o'],
 //     board: [
@@ -10,57 +15,45 @@
 //       [null, null, null]
 //     ]
 //   }
+
 // DOM Selectors Areas
 const boardElement = document.getElementsByClassName("board");
 const vsHumanElement = document.getElementById("vsHuman");
-const vsComputerElement = document.getElementById("vsComputer")
+const vsComputerElement = document.getElementById("vsComputer");
 
-///DOM Selectors Forms 
-// const player1Input = document.getElementById("enterPlayer1").value;
+// DOM Selectors Forms 
+const player1Input = document.getElementById("enterPlayer1").value;
 const player2Input = document.getElementById("enterPlayer2").value;
-
 
 /// DOM Selector Buttons
 const playComputerButton = document.getElementById("playComputer");
 const playHumanButton = document.getElementById("playHuman");
 
-//Form Functions
-
-function displayPlayer1() {
-    let player1Input = document.getElementById("enterPlayer1").value;
-    alert("The name of the text field is: " + player1Input);
-
-}
-
-// const Player1Name = () => {
-//     return player1Input.innerHTML
-// }
-
-// const Player2Name = () => {
-//     player2Input = document.getElementById("displayPlayer1").innerHTML
-// }
 
 
+///Button and Form Functions
 
-///Button Functions
+const displayPlayers = () => {
+    let player1Name = document.getElementById("enterPlayer1").value;
+    let player2Name = document.getElementById("enterPlayer2").value;
+    let playersDisplay = "Player 1 is " + player1Name + " and Player 2 is " + player2Name;
+    alert (playersDisplay) }
 
 const playHumanClick = () => {
     if (vsComputerElement.style.display === "none"){
     vsComputerElement.style.display = "block";}
-    else {vsComputerElement.style.display = "none"
-}
-}
+    else {vsComputerElement.style.display = "none"}}
 
 const playComputerClick = () => {
     if (vsHumanElement.style.display === "none"){
         vsHumanElement.style.display = "block";}
-        else {vsHumanElement.style.display = "none"
-    }
-    }
+        else {vsHumanElement.style.display = "none"}}
 
 ///Event Listeners
 playHumanButton.addEventListener("click", playHumanClick);
-playComputerButton.addEventListener("click", playComputerClick)
+playComputerButton.addEventListener("click", playComputerClick);
+vsHumanSubmit.addEventListener("click", displayPlayers);
+
 
 
 
