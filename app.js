63313ab58a6360cbc513a9ca9bc37346d0118e79
tbrown034 <tbrown034@gmail.com>
2022-09-01 23:06:03 -0500

    writing x

--- app.js
+++ app.js
@@ -1,63 +1,65 @@
-// state
+// state and constants
 const state = {};
-const resetState = {
-    board: [
-      [null, null, null],
-      [null, null, null],
-      [null, null, null]
-    ] 
+const resetState = () => {
+  state.board = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]]
+  players: ['xPlayer', 'oPlayer']
   }
+
+let currentPlayer = 'x'
 
 const game = {
     xTurn: true,
     xState: [],
     oState: [],
   }
-  
 
-  // Note: Need to understand why/how this works better
-  document.addEventListener('click', event => { 
-    const target = event.target;
-    const cellValue = target.dataset.value;
-    game.xTurn === true
-        ? game.xState.push(cellValue)
-        : game.oState.push(cellValue)
-        target.classList.add('disabled')
-        target.classList.add(game.xTurn ? 'x' : 'o')
-        game.xTurn = !game.xTurn   
-    }
-  )
+  const renderBoard = () => {}
+  const resetBoard = () => {    
+  }
 
+  const resetAll = () => {
+      window.location.reload()
+  }
 // DOM Selectors Areas
-const boardElement = document.getElementsByClassName("board");
+const boardElement = document.getElementById("board");
 const vsHumanElement = document.getElementById("vsHuman");
 const vsComputerElement = document.getElementById("vsComputer");
-
+const cellElement = document.getElementsByClassName("cell")
 // DOM Selectors Forms 
 const playerNames = document.getElementById("vsHumanForm").value;
 const player1Name = document.getElementById("enterPlayer1").value;
 const player2Name = document.getElementById("enterPlayer2").value;
 const playerSoloName = document.getElementById("enterPlayerSolo").value;
-
 /// DOM Selector Buttons
 const playComputerButton = document.getElementById("playComputer");
 const playHumanButton = document.getElementById("playHuman");
+const resetButton = document.getElementById("resetButton");
 
-///Button and Form Functions
+// Clickable cells
+boardElement.addEventListener("click", event => {
+  if (event.target.innerHTML == 'x' || event.target.innerHTML == 'o'){
+  return
+  }
+  if (xTurn=true) {
+    event.target.innerHTML = "x";
+  }
+  if (xTurn=false) {
+    event.target.innerHTML = "o";
+  }})
+// Who's turn
 
-const resetBoard = () => {    
-}
+whoseTurnvsPlayers =
+  
 
-const resetAll = () => {
-    window.location.reload()
-}
 
 function displayPlayersFunction () {
     let player1Name = document.getElementById("enterPlayer1").value;
     let player2Name = document.getElementById("enterPlayer2").value;
     document.getElementById("playerNames").innerText = "Welcome, " + player1Name + " and " + player2Name + "! Let's get ready to play! Good luck to you both!";
    }
-
    function displayPlayerFunction () {
     let playerSoloName = document.getElementById("enterPlayerSolo").value;
     document.getElementById("playerName").innerText = "Welcome, " + playerSoloName + "! Let's get ready to play the computer! Good luck to you!";
@@ -78,5 +80,11 @@ const playComputerClick = () => {
 playHumanButton.addEventListener("click", playHumanClick);
 playComputerButton.addEventListener("click", playComputerClick);
 resetButton.addEventListener("click", playComputerClick);
+// document.addEventListener("click", event => {
+//   const target = event.target;
+//   target.classList.add(game.xTurn ? 'x' : 'o')
+// })
 
+
+// boostrapping
 
