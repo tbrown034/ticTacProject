601f63e9464d8126949221e81bd9b89b59ef8612
tbrown034 <tbrown034@gmail.com>
2022-09-02 14:42:01 -0500

    the board actually works cant check for winners though

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
 // state and constants
 const state = {};
 const resetState = () => {
+  state.players = ['xPlayer', 'oPlayer']
   state.board = [
     [null, null, null],
     [null, null, null],
-    [null, null, null]]
-  players: ['xPlayer', 'oPlayer']
-  }
+    [null, null, null],
+  ]}
+  const statusDisplay = document.querySelector('.game--status');
+  let currentPlayer = "xPlayer";
+  const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+  statusDisplay.innerHTML = currentPlayerTurn();
 
-let currentPlayer = 'x'
 
-const game = {
-    xTurn: true,
-    xState: [],
-    oState: [],
-  }
 
-  const renderBoard = () => {}
-  const resetBoard = () => {    
-  }
+  const playerChange = () => {
+    if (currentPlayer === 'xPlayer') {
+      currentPlayer = 'oPlayer'}
+      else {currentPlayer = 'xPlayer'}
+      statusDisplay.innerHTML = currentPlayerTurn()
+    }
+    // currentPlayer = currentPlayer === "xPlayer" ? "oPlayer" : "xPlayer"
+    // statusDisplay.innerHTML = currentPlayerTurn();
+  
+
+
+
 
   const resetAll = () => {
       window.location.reload()
@@ -33,36 +40,41 @@ const playerNames = document.getElementById("vsHumanForm").value;
 const player1Name = document.getElementById("enterPlayer1").value;
 const player2Name = document.getElementById("enterPlayer2").value;
 const playerSoloName = document.getElementById("enterPlayerSolo").value;
-/// DOM Selector Buttons
+/// DOM Selector 
 const playComputerButton = document.getElementById("playComputer");
 const playHumanButton = document.getElementById("playHuman");
 const resetButton = document.getElementById("resetButton");
+
+
 
 // Clickable cells
 boardElement.addEventListener("click", event => {
   if (event.target.innerHTML == 'x' || event.target.innerHTML == 'o'){
   return
   }
-  if (xTurn=true) {
+  if (currentPlayer === 'xPlayer') {
     event.target.innerHTML = "x";
-  }
-  if (xTurn=false) {
+    playerChange()
+    console.log("x", event.target, currentPlayer)  
+    }
+  else {
     event.target.innerHTML = "o";
+    playerChange()
+    console.log("o", event.target, currentPlayer)
+    console.log("after", event.target, currentPlayer)
+   
   }})
-// Who's turn
 
-whoseTurnvsPlayers =
-  
 
 
 function displayPlayersFunction () {
     let player1Name = document.getElementById("enterPlayer1").value;
     let player2Name = document.getElementById("enterPlayer2").value;
-    document.getElementById("playerNames").innerText = "Welcome, " + player1Name + " and " + player2Name + "! Let's get ready to play! Good luck to you both!";
+    document.getElementById("playerNames").innerText = "Welcome, " + player1Name + " and " + player2Name + "! Let's get ready to play! " + player1Name + " will go first as X. " + player2Name + " will go second as O. Good luck to you both!";
    }
    function displayPlayerFunction () {
     let playerSoloName = document.getElementById("enterPlayerSolo").value;
-    document.getElementById("playerName").innerText = "Welcome, " + playerSoloName + "! Let's get ready to play the computer! Good luck to you!";
+    document.getElementById("playerName").innerText = "Welcome, " + playerSoloName + "! Let's get ready to play the computer! " + playerSoloName + " you'll be starting first as X. Good luck to you!";
    }
 
 const playHumanClick = () => {
@@ -87,4 +99,4 @@ resetButton.addEventListener("click", playComputerClick);
 
 
 // boostrapping
-
+resetState()
