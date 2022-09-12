8e1a28ace946871eca20d5adf9419527b1c793d1
tbrown034 <tbrown034@gmail.com>
2022-09-12 13:09:05 -0500

    test

--- app.js
+++ app.js
@@ -1,40 +1,35 @@
 // state and constants
-const state = {};
-const resetState = () => {
-  state.players = ['xPlayer', 'oPlayer']
-  state.board = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-  ]}
-  const statusDisplay = document.querySelector('.game--status');
-  let currentPlayer = "xPlayer";
-  const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
-  statusDisplay.innerHTML = currentPlayerTurn();
+let initialState;
+const renderState = () => {
 
-
-
-  const playerChange = () => {
-    if (currentPlayer === 'xPlayer') {
-      currentPlayer = 'oPlayer'}
-      else {currentPlayer = 'xPlayer'}
-      statusDisplay.innerHTML = currentPlayerTurn()
-    }
-    // currentPlayer = currentPlayer === "xPlayer" ? "oPlayer" : "xPlayer"
-    // statusDisplay.innerHTML = currentPlayerTurn();
-  
+}
 
 
 
 
-  const resetAll = () => {
-      window.location.reload()
-  }
+let gameState = ["", "", "", "", "", "", "", "", ""];
+const statusDisplay = document.querySelector('.game--status');
+let currentPlayer = "xPlayer";
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+statusDisplay.innerHTML = currentPlayerTurn();
+const winningMessage = () => `Player ${currentPlayer} has won!`;
+const drawMessage = () => `Game ended in a draw!`;
+const winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
 // DOM Selectors Areas
 const boardElement = document.getElementById("board");
 const vsHumanElement = document.getElementById("vsHuman");
 const vsComputerElement = document.getElementById("vsComputer");
-const cellElement = document.getElementsByClassName("cell")
+const cellElement = document.getElementsByClassName("cell").value
+const cellEle = document.querySelectorAll(".cell")
 // DOM Selectors Forms 
 const playerNames = document.getElementById("vsHumanForm").value;
 const player1Name = document.getElementById("enterPlayer1").value;
@@ -43,10 +38,17 @@ const playerSoloName = document.getElementById("enterPlayerSolo").value;
 /// DOM Selector 
 const playComputerButton = document.getElementById("playComputer");
 const playHumanButton = document.getElementById("playHuman");
-const resetButton = document.getElementById("resetButton");
+// const resetButton = document.getElementById("resetButton");
+// const resetButton = document.querySelectorAll("#resetButton")
 
-
-
+//// Helper Functions
+// Turns
+const playerChange = () => {
+  if (currentPlayer === 'xPlayer') {
+    currentPlayer = 'oPlayer'}
+    else {currentPlayer = 'xPlayer'}
+    statusDisplay.innerHTML = currentPlayerTurn()
+  }
 // Clickable cells
 boardElement.addEventListener("click", event => {
   if (event.target.innerHTML == 'x' || event.target.innerHTML == 'o'){
@@ -55,48 +57,39 @@ boardElement.addEventListener("click", event => {
   if (currentPlayer === 'xPlayer') {
     event.target.innerHTML = "x";
     playerChange()
-    console.log("x", event.target, currentPlayer)  
     }
   else {
     event.target.innerHTML = "o";
     playerChange()
-    console.log("o", event.target, currentPlayer)
-    console.log("after", event.target, currentPlayer)
-   
   }})
-
-
-
+//Restart  
+  const resetAll = () => {
+    currentPlayer = "xPlayer";
+    gameState = ["", "", "", "", "", "", "", "", ""];
+    statusDisplay.innerHTML = currentPlayerTurn();
+    document.querySelectorAll(".cell").forEach(cell => cell.innerHTML = "");
+  }
+//Name or Display Functions
 function displayPlayersFunction () {
     let player1Name = document.getElementById("enterPlayer1").value;
     let player2Name = document.getElementById("enterPlayer2").value;
     document.getElementById("playerNames").innerText = "Welcome, " + player1Name + " and " + player2Name + "! Let's get ready to play! " + player1Name + " will go first as X. " + player2Name + " will go second as O. Good luck to you both!";
    }
-   function displayPlayerFunction () {
+function displayPlayerFunction () {
     let playerSoloName = document.getElementById("enterPlayerSolo").value;
     document.getElementById("playerName").innerText = "Welcome, " + playerSoloName + "! Let's get ready to play the computer! " + playerSoloName + " you'll be starting first as X. Good luck to you!";
    }
-
+//Button Functions
 const playHumanClick = () => {
     if (vsComputerElement.style.display === "none"){
     vsComputerElement.style.display = "block";}
     else {vsComputerElement.style.display = "none"}}
-
 const playComputerClick = () => {
     if (vsHumanElement.style.display === "none"){
         vsHumanElement.style.display = "block";}
         else {vsHumanElement.style.display = "none"}}
-
 ///Event Listeners
-
 playHumanButton.addEventListener("click", playHumanClick);
 playComputerButton.addEventListener("click", playComputerClick);
 resetButton.addEventListener("click", playComputerClick);
-// document.addEventListener("click", event => {
-//   const target = event.target;
-//   target.classList.add(game.xTurn ? 'x' : 'o')
-// })
-
-
 // boostrapping
-resetState()
